ce17375ccba3b85d8be695b4799d836b76350d18
Madsfredag <43322344+Madsfredag@users.noreply.github.com>
2023-05-09 11:29:43 +0200

    add CreateAnnonce component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,34 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
- // Route,
+  // Route,
 } from "react-router-dom";
-import {Search} from "./Components/Search.js";
-import {Navbar} from "./Components/Navbar.js";
+import { Search } from "./Components/Search.js";
+import { Navbar } from "./Components/Navbar.js";
 
 import "./App.css"
+import { CreateAnnonce } from "./Components/CreateAnnonce.js";
 
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        
-      </Routes>
-    <div className="main">
-      <Search />
-    </div>
-    </Router>
+    // <><Router>
+    //   <Navbar />
+    //   <Routes>
+
+    //   </Routes>
+    //   <div className="main">
+    //     <Search />
+    //   </div>
+    // </Router>
+      <CreateAnnonce />
+    // </>
 
 
 
 
   );
-  
+
 }
 
 export default App;
---
+++ frontend/src/Components/CreateAnnonce.js
@@ -0,0 +1,67 @@
+import react, { useState } from 'react'
+
+export function CreateAnnonce() {
+    const [price, setPrice] = useState(0);
+    const [titel, setTitel] = useState('');
+    const [kategori, setKategori] = useState('');
+    const [beskrivelse, setBeskrivelse] = useState('');
+    const [studieretning, setStudieretning] = useState('');
+    const [billedsti, setBilledsti] = useState('');
+
+    const handleSubmit = () => {
+        postAnnonce()
+    };
+
+    const postAnnonce = async () => {
+        try {
+            const response = await fetch('https://localhost:7163/api/Annonces', {
+                method: 'POST',
+                headers: {
+
+                },
+                body: JSON.stringify({
+                    Price: price,
+                    Titel: titel,
+                    Kategori: kategori,
+                    Beskrivelse: beskrivelse,
+                    Studieretning: studieretning,
+                    BilledSti: billedsti
+                })
+            });
+            if (!response.ok) {
+                throw new Error('couldnt post ad');
+            };
+            const data = await response.json();
+            console.log('data received:', data);
+        }
+        catch (error) {
+            console.log('Error:  ', error);
+        }
+    };
+
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="price">Pris:</label>
+            <input type="number" id="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+
+            <label htmlFor="title">Titel:</label>
+            <input type="text" id="title" value={titel} onChange={(e) => setTitel(e.target.value)} />
+
+            <label htmlFor="category">Kategori:</label>
+            <input type="text" id="category" value={kategori} onChange={(e) => setKategori(e.target.value)} />
+
+            <label htmlFor="description">Beskrivelse:</label>
+            <textarea id="description" value={beskrivelse} onChange={(e) => setBeskrivelse(e.target.value)} />
+
+            <label htmlFor="major">Studieretning:</label>
+            <input type="text" id="major" value={studieretning} onChange={(e) => setStudieretning(e.target.value)} />
+
+            <label htmlFor="image">Billedsti:</label>
+            <input type="text" id="image" value={billedsti} onChange={(e) => setBilledsti(e.target.value)} />
+
+            <button type="submit">OPRET ANNONCE!</button>
+        </form>
+    )
+}
