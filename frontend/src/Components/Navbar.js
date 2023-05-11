ad1c32ea8e6fdf3c375983bd9603c77dbf6f02af
Jcc1337 <123932310+Jcc1337@users.noreply.github.com>
2023-05-11 12:19:46 +0200

    .

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -17,16 +17,6 @@ export function Navbar() {
       <NavLink to="/Register" className="nav_button" activeclassname="nav_button_active">
         Opret Bruger
       </NavLink>
-
-      {/* <NavLink to="/" className="nav_button_sale" exact activeClassName="nav_button_active" >
-        Sælg dine bøger
-      </NavLink>
-      <NavLink to="/Login" className="nav_button" activeClassName="nav_button_active">
-        Login
-      </NavLink>
-      <NavLink to="/Register" className="nav_button" activeClassName="nav_button_active">
-        Opret Bruger
-      </NavLink> */}
       </div>
     </nav>
   )
