96ba647a742ab90a77f7f5534eeabf201ef6cc50
JonasKnudsen <jonasfbknudsen@gmail.com>
2023-05-17 14:58:46 +0200

    Post ad works

--- frontend/src/Components/CreateAnnonce.js
+++ frontend/src/Components/CreateAnnonce.js
@@ -1,82 +1,140 @@
-import react, { useState } from 'react'
+import react, { useState } from "react";
+import "./CSS/Annonce.css";
 
 export function CreateAnnonce() {
-    const [price, setPrice] = useState(0);
-    const [titel, setTitel] = useState('');
-    const [kategori, setKategori] = useState('');
-    const [beskrivelse, setBeskrivelse] = useState('');
-    const [studieretning, setStudieretning] = useState('');
-    const [billedesti, setBilledsti] = useState('');
-    const [efManagerId, setEfManagerId]=useState('');
-    const [stand, setStand]=useState('');
-    const [chatId, setChatId]=useState('');
+  const [price, setPrice] = useState("");
+  const [titel, setTitel] = useState("");
+  const [kategori, setKategori] = useState("");
+  const [beskrivelse, setBeskrivelse] = useState("");
+  const [studieretning, setStudieretning] = useState("");
+  const [billedesti, setBilledsti] = useState("");
+  const [efManagerId, setEfManagerId] = useState("");
+  const [stand, setStand] = useState("");
+  const [chatId, setChatId] = useState("");
 
-    const handleSubmit = () => {
-        postAnnonce()
-    };
+  const handleSubmit = () => {
+    postAnnonce();
+  };
 
-    const postAnnonce = async () => {
-        try {
-            const response = await fetch('https://localhost:7181/api/Annonces', {
-                method: 'POST',
-                headers: {
+  const postAnnonce = async () => {
+    try {
+      const response = await fetch("https://localhost:7181/api/Annonces", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          Price: price,
+          Titel: titel,
+          Kategori: kategori,
+          Beskrivelse: beskrivelse,
+          Studieretning: studieretning,
+          BilledeSti: billedesti,
+          EfManagerId: efManagerId,
+          Stand: stand,
+          ChatId: chatId,
+        }),
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("couldnt post ad");
+      }
+      const data = await response.json();
+      console.log("data received:", data);
+    } catch (error) {
+      console.log("Error:  ", error);
+    }
+  };
 
-                },
-                body: JSON.stringify({
-                    Price: price,
-                    Titel: titel,
-                    Kategori: kategori,
-                    Beskrivelse: beskrivelse,
-                    Studieretning: studieretning,
-                    BilledeSti: billedesti,
-                    EfManagerId: efManagerId,
-                    Stand: stand,
-                    ChatId: chatId
-                })
-            });
-            if (!response.ok) {
-                throw new Error('couldnt post ad');
-            };
-            const data = await response.json();
-            console.log('data received:', data);
-        }
-        catch (error) {
-            console.log('Error:  ', error);
-        }
-    };
+  return (
+    <div className="form-border">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-input"
+          type="number"
+          id="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="Indsæt pris"
+        />
 
+        <input
+          className="form-input"
+          type="text"
+          id="title"
+          value={titel}
+          onChange={(e) => setTitel(e.target.value)}
+          placeholder="Indsæt titel"
+        />
 
+        <input
+          className="form-input"
+          type="text"
+          id="category"
+          value={kategori}
+          onChange={(e) => setKategori(e.target.value)}
+          placeholder="Indsæt kategori"
+        />
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="price">Pris:</label>
-            <input type="number" id="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+        <textarea
+          className="form-textarea"
+          id="description"
+          value={beskrivelse}
+          onChange={(e) => setBeskrivelse(e.target.value)}
+          placeholder="Indsæt beskrivelse"
+        ></textarea>
 
-            <label htmlFor="title">Titel:</label>
-            <input type="text" id="title" value={titel} onChange={(e) => setTitel(e.target.value)} />
+        <input
+          className="form-input"
+          type="text"
+          id="major"
+          value={studieretning}
+          onChange={(e) => setStudieretning(e.target.value)}
+          placeholder="Indsæt studieretning"
+        />
 
-            <label htmlFor="category">Kategori:</label>
-            <input type="text" id="category" value={kategori} onChange={(e) => setKategori(e.target.value)} />
+        <input
+          className="form-input"
+          type="text"
+          id="image"
+          value={billedesti}
+          onChange={(e) => setBilledsti(e.target.value)}
+          placeholder="Indsæt billedesti"
+        />
 
-            <label htmlFor="description">Beskrivelse:</label>
-            <textarea id="description" value={beskrivelse} onChange={(e) => setBeskrivelse(e.target.value)} />
+        <input
+          className="form-input"
+          type="number"
+          id="ManagerId"
+          value={efManagerId}
+          onChange={(e) => setEfManagerId(e.target.value)}
+          placeholder="Indsæt EfManagerId"
+        />
 
-            <label htmlFor="major">Studieretning:</label>
-            <input type="text" id="major" value={studieretning} onChange={(e) => setStudieretning(e.target.value)} />
+        <select
+          className="form-input"
+          id="Condition"
+          value={stand}
+          onChange={(e) => setStand(e.target.value)}
+        >
+          <option value="">Vælg stand</option>
+          <option value="Som ny">Som ny</option>
+          <option value="Lidt slidt">Lidt slidt</option>
+          <option value="Slidt">Slidt</option>
+          <option value="Velbrugt">Velbrugt</option>
+        </select>
 
-            <label htmlFor="image">Billedesti:</label>
-            <input type="text" id="image" value={billedesti} onChange={(e) => setBilledsti(e.target.value)} />
+        <input
+          className="form-input"
+          type="number"
+          id="ChatId"
+          value={chatId}
+          onChange={(e) => setChatId(e.target.value)}
+          placeholder="Indsæt ChatId"
+        />
 
-            <label htmlFor="ManagerId">EfManagerId:</label>
-            <input type="number" id="ManagerId" value={efManagerId} onChange={(e) => setEfManagerId(e.target.value)} />
-
-            <label htmlFor="Condition">Stand:</label>
-            <input type="text" id="Condition" value={stand} onChange={(e) => setStand(e.target.value)} />
-
-            <label htmlFor="ChatId">ChatId:</label>
-            <input type="number" id="ChatId" value={chatId} onChange={(e) => setChatId(e.target.value)} />
-
-            <button type="submit">OPRET ANNONCE!</button>
-        </form>
-    )
+        <button className="submitbutton" type="submit">
+          OPRET ANNONCE!
+        </button>
+      </form>
+    </div>
+  );
 }
