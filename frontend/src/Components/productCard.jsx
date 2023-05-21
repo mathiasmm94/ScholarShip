2b641666329533633bd9ccf1b4f82fff2cf9b146
Madsfredag <43322344+Madsfredag@users.noreply.github.com>
2023-05-21 12:12:55 +0200

    temp

--- frontend/src/Components/productCard.jsx
+++ frontend/src/Components/productCard.jsx
@@ -1,22 +1,21 @@
-// import Card from "@mui/material/Card";
-// import Image from "mui-image";
-// import Stack from "@mui/material/Stack";
-// import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
-// export function ProductCard({ Title, ImgSource, category, price, sx }) {
-//   return (
-//     <Card sx={sx}>
-//       <Image src={ImgSource} sx={sx} />
-//       <Stack spacing={2}>
-//         <Stack direction={'row'} sx={{justifyContent: 'space-between'}}>
-//           <Typography>{Title}</Typography>
-//           <Typography>{price}</Typography>
-//         </Stack>
-//         <Stack direction={'row'} sx={{justifyContent: 'space-between'}}>
-//           <Typography>{category}</Typography>
-//           <Typography>{price}</Typography>
-//         </Stack>
-//       </Stack>
-//     </Card>
-//   );
-// }
+export function ProductCard({ Title, ImgSource, category, price, studieRetning, stand }) {
+  return (
+    <Card variant={'elevation'} sx={{padding: '10px', margin: '10px'}} >
+      <Stack spacing={10} direction={'row'}>
+        
+      <img alt="produktbillede" src={ImgSource} width={'150px'} />
+        <Stack  spacing={12} direction={'row'} sx={{justifyContent: 'space-between'}}>
+        <div>Titel på bogen: <Typography fontWeight={'bold'}> {Title}</Typography></div>
+        <div>Pris: <Typography fontWeight={'bold'}> {price} DKK.</Typography></div>
+            <div>Kategori: <Typography fontWeight={'bold'}> {category}</Typography></div>
+            <div>Studieretning: <Typography fontWeight={'bold'}> {studieRetning}</Typography></div>
+            <div>Varens stand: <Typography fontWeight={'bold'}> {stand}</Typography></div>
+        </Stack>
+      </Stack>
+    </Card>
+  );
+}
